1c25c220f97b8f162dd16925001bbf01b23621ff
Arghdeep-hub <mallickarghadeep1@gmail.com>
2023-05-03 00:17:40 +0530

    added services worker

--- next.config.js
+++ next.config.js
@@ -2,14 +2,12 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  // disable: process.env.NODE_ENV === "development",
 });
 
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
-
-module.exports = nextConfig;
+});
