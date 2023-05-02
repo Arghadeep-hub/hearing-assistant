54abca8a6eb5b07b84475853e48f570a38f86bdb
Arghdeep-hub <mallickarghadeep1@gmail.com>
2023-05-02 23:35:03 +0530

    ios and android module added

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"></link>
+        <meta name="theme-color" content="#fff" />
+        <link
+          rel="shortcut icon"
+          href="/icons/favicon.png"
+          type="image/x-icon"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
